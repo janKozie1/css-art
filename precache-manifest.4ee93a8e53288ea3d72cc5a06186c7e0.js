dee5c872a856037a8fda0a87c606a9e75063b9d3
Jan Kozieł <48291123+janKozie1@users.noreply.github.com>
2020-04-16 20:06:29 +0200

    Updates

---
+++ precache-manifest.4ee93a8e53288ea3d72cc5a06186c7e0.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "21512e81e45aa64e54f152f3ae46564a",
+    "url": "/css-art/index.html"
+  },
+  {
+    "revision": "7f35a1ceb207ee23d8f4",
+    "url": "/css-art/static/js/2.043995cd.chunk.js"
+  },
+  {
+    "revision": "04740fca020b597fe367",
+    "url": "/css-art/static/js/main.d634feaa.chunk.js"
+  },
+  {
+    "revision": "c6faad555a6a8cafa647",
+    "url": "/css-art/static/js/runtime~main.8c748e3c.js"
+  }
+]);
